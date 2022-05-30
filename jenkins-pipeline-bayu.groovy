fb853bbfa40a4c8c1f2ecc7e250cc3d513061f0d
bayukrsn <96294690+bayukrsn@users.noreply.github.com>
2022-05-30 13:56:59 +0700

    master

---
+++ jenkins-pipeline-bayu.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+    stages {
+        stage('git clone') {
+            steps {
+                sh 'git clone --branch=master https://user:password@gitlab-saya.com/repo/project.git'
+            }
+        }
+        stage('build') {
+            steps {
+                sh  '''
+                    cd /home/bayu/bayu-springboot/bayu-example-spring-boot
+                    docker build -t bayu-springboot:v3 .
+                    '''
+            }
+        }
+        stage('push to container registry') {
+            steps {
+                sh "docker push bayukrsn/bayu-springboot:v3"
+            }
+        }
+        stage('deploy app') {
+            steps {
+                sh  ''' 
+                    kubectl apply bayu-springboot-deployment.yaml
+                    kubectl apply bayu-springboot-secret.yaml -n development
+                    kubectl apply bayu-springboot-service.yaml -n development
+                    kubectl apply bayu-springboot-ingress.yaml -n development
+                    '''
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+            }
+        }
+}
